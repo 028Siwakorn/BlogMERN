fe8444323996afbb8f24e8af529ee29e1b10024d
Siwakorn Kleebmek <664259028@webmail.npru.ac.th>
2025-12-01 12:09:34 +0700

    FirstPrototype

---
+++ src/routes/router.jsx
@@ -0,0 +1,37 @@
+import { createBrowserRouter } from "react-router";
+import CreatePost from "../pages/CreatePost";
+import EditPost from "../pages/EditPost";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import HomePage from "../pages/HomePage";
+import MainLayouts from "../layouts/MainLayouts";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayouts />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/createpost",
+        element: <CreatePost />,
+      },
+      {
+        path: "/editpost",
+        element: <EditPost />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+export default router;
